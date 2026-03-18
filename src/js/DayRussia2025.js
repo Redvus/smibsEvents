f39b64f4d36328cc1ec0c6c3abef8bdc35e90666
Redvus <a.suvorof@gmail.com>
2026-03-18 11:07:41 +0400

    Сгруппировал события в одном месте.

---
+++ src/js/DayRussia2025.js
@@ -0,0 +1,186 @@
+// import { gsap } from 'gsap'; // Убрать в продакшене
+
+export class DayRussia2025 {
+	constructor() {
+		this.initLayout();
+		// this.initAnimation();
+		// this.initAnimationMobile();
+		this.initMobile();
+		// this.initDev();
+	}
+
+	initLayout() {
+		this.bodyBlock = document.body;
+		this.dayRussia2025 = document.createElement('div');
+		// this.container = document.createElement('div');
+
+		this.dayRussia2025.id = 'dayRussia2025';
+		this.dayRussia2025.className = 'anniversary anniversary--dayRussia2025';
+		// this.container.classList = 'anniversary__container';
+
+		// this.dayRussia2025.innerHTML = `
+		// 	<picture id="dayRussia2025Baloon">
+		// 		<img src="/assets/images/dayRussia/dayRussia2025/dayRussia2025_baloon.png" alt="">
+		// 	</picture>
+		// 	<picture id="dayRussia2025Girl">
+		// 		<img src="/assets/images/dayRussia/dayRussia2025/dayRussia2025_girl.jpg" alt="">
+		// 	</picture>
+		// 	<picture id="dayRussia2025Slogan_1">
+		// 		<img src="/assets/images/dayRussia/dayRussia2025/dayRussia2025_slogan_1.png" alt="">
+		// 	</picture>
+		// 	<picture id="dayRussia2025Slogan_2">
+		// 		<img src="/assets/images/dayRussia/dayRussia2025/dayRussia2025_slogan_2.png" alt="">
+		// 	</picture>
+		// 	<picture id="dayRussia2025Slogan_3">
+		// 		<img src="/assets/images/dayRussia/dayRussia2025/dayRussia2025_slogan_3.png" alt="">
+		// 	</picture>
+		// `;
+
+		this.dayRussia2025.innerHTML = `
+			<picture id="dayRussia2025Baloon">
+				<img src="/assets/images/dayRussia/dayRussia2025/dayRussia2025_baloon.png" alt="">
+			</picture>
+			<picture id="dayRussia2025Girl">
+				<img src="/assets/images/dayRussia/dayRussia2025/dayRussia2025_girl.jpg" alt="">
+			</picture>
+			<picture id="dayRussia2025Slogan_1">
+				<img src="/assets/images/dayRussia/dayRussia2025/dayRussia2025_slogan_1.png" alt="">
+			</picture>
+			<picture id="dayRussia2025Slogan_2">
+				<img src="/assets/images/dayRussia/dayRussia2025/dayRussia2025_slogan_4.png" alt="">
+			</picture>
+		`;
+
+		// Appends
+		this.bodyBlock.appendChild(this.dayRussia2025);
+	}
+
+	initAnimation(delay = 0.6) {
+		const
+			dayRussia2025 = document.getElementById('dayRussia2025'),
+			dayRussia2025Baloon = document.getElementById('dayRussia2025Baloon'),
+			dayRussia2025Girl = document.getElementById('dayRussia2025Girl'),
+			dayRussia2025Slogan_1 = document.getElementById('dayRussia2025Slogan_1'),
+			dayRussia2025Slogan_2 = document.getElementById('dayRussia2025Slogan_2'),
+			dayRussia2025Slogan_3 = document.getElementById('dayRussia2025Slogan_3')
+		;
+
+		let tl = new gsap.timeline({
+			delay: delay,
+			onComplete: this.initHide(7)
+		});
+
+		tl
+			.to(dayRussia2025, {
+				duration: 0.3,
+				autoAlpha: 1,
+				zIndex: 9999,
+				// easy: "elastic.in(1,0.3)"
+			})
+			.from(dayRussia2025Girl, {
+				duration: 1,
+				delay: '0.2',
+				autoAlpha: 0,
+				// y: '-10%'
+			})
+			.from(dayRussia2025Baloon, {
+				duration: 0.6,
+				delay: '-0.5',
+				autoAlpha: 0,
+				x: '-10%'
+			})
+			.from([dayRussia2025Slogan_1, dayRussia2025Slogan_2, dayRussia2025Slogan_3], {
+				duration: 0.6,
+				delay: '-0.3',
+				autoAlpha: 0,
+				x: '7%',
+				stagger: 0.3
+			})
+		;
+	}
+
+	initAnimationMobile(delay = 0.6) {
+		const
+			dayRussia2025 = document.getElementById('dayRussia2025'),
+			dayRussia2025Baloon = document.getElementById('dayRussia2025Baloon'),
+			dayRussia2025Girl = document.getElementById('dayRussia2025Girl'),
+			dayRussia2025Slogan_1 = document.getElementById('dayRussia2025Slogan_1'),
+			dayRussia2025Slogan_2 = document.getElementById('dayRussia2025Slogan_2'),
+			dayRussia2025Slogan_3 = document.getElementById('dayRussia2025Slogan_3')
+		;
+
+		let tl = new gsap.timeline({
+			delay: delay,
+			onComplete: this.initHide(7)
+		});
+
+		tl
+			.to(dayRussia2025, {
+				duration: 0.3,
+				autoAlpha: 1,
+				zIndex: 9999,
+				// easy: "elastic.in(1,0.3)"
+			})
+			.from(dayRussia2025Girl, {
+				duration: 1,
+				delay: '0.2',
+				autoAlpha: 0,
+				// y: '-10%'
+			})
+			.from([dayRussia2025Slogan_1, dayRussia2025Slogan_2, dayRussia2025Slogan_3], {
+				duration: 0.6,
+				delay: '-0.3',
+				autoAlpha: 0,
+				y: '10%',
+				stagger: 0.3
+			})
+		;
+	}
+
+	initHide(delay) {
+		let tl = new gsap.timeline({
+			delay: delay,
+			onComplete: () => {
+				setTimeout(() => {
+					this.bodyBlock.removeChild(this.dayRussia2025);
+				}, 1000)
+			}
+		});
+
+		tl
+			.to(this.dayRussia2025, {
+				autoAlpha: 0,
+				duration: 0.6,
+				delay: '-0.8',
+				display: 'none',
+				zIndex: '-1',
+				ease: 'power1'
+			})
+		;
+	}
+
+	initDev() {
+		this.dayRussia2025.style.opacity = 1;
+		this.dayRussia2025.style.visibility = 'visible';
+		this.dayRussia2025.style.zIndex = 9999;
+	}
+
+	initMobile() {
+
+		screen.orientation.addEventListener('change', () => {
+			if (screen.orientation.type === 'landscape-primary' || screen.orientation.type === 'landscape-secondary') {
+				location.reload();
+			} else if (screen.orientation.type === 'portrait-primary' || screen.orientation.type === 'portrait-secondary') {
+				location.reload();
+			}
+		});
+
+		if (screen.orientation.type === 'landscape-primary' || screen.orientation.type === 'landscape-secondary') {
+			this.initAnimation();
+		} else if (screen.orientation.type === 'portrait-primary' || screen.orientation.type === 'portrait-secondary') {
+			this.initAnimationMobile();
+		} else {
+			this.initAnimation();
+		}
+	}
+}
---
+++ src/js/Event_08_03_26.js
@@ -0,0 +1,133 @@
+import { gsap } from "gsap";
+
+class Event_08_03_26 {
+    constructor() {
+        this.initLayout();
+        if (import.meta.env.PROD) {
+            this.initAnimation();
+            // this.initDev();
+        }
+        if (import.meta.env.DEV) {
+            // this.initAnimation();
+            this.initDev();
+        }
+    }
+
+    initLayout() {
+        this.bodyBlock = document.body;
+        this.womanDay26 = document.createElement("div");
+        this.container = document.createElement("div");
+
+        this.womanDay26.id = "womanDay26";
+        this.womanDay26.className = "anniversary anniversary--womanDay26";
+        this.container.classList = "anniversary__container";
+
+        this.womanDay26.innerHTML = `
+			<picture id="womanDay26Back">
+				<img src="assets/plugins/womanDay26/images/womanDay26Back.jpg" alt="">
+			</picture>
+            <picture id="womanDay26Baloon">
+				<img src="assets/plugins/womanDay26/images/womanDay26Baloon_2.png" alt="">
+			</picture>
+		`;
+
+        // this.container.innerHTML = `
+        //     <div id="womanDay26Slogan">
+        //         <picture id="womanDay26Slogan_1">
+        //             <img src="assets/plugins/womanDay26/images/womanDay26Slogan_1.svg" id="womanDay26Slogan_1" alt="">
+        //         </picture>
+        //         <picture id="womanDay26Slogan_2">
+        //             <img src="assets/plugins/womanDay26/images/womanDay26Slogan_2.svg" id="womanDay26Slogan_2" alt="">
+        //         </picture>
+        //         <picture id="womanDay26Slogan_3">
+        //             <img src="assets/plugins/womanDay26/images/womanDay26Slogan_3.svg" id="womanDay26Slogan_3" alt="">
+        //         </picture>
+        //     </div>
+        // `;
+
+        this.container.innerHTML = `
+            <div id="womanDay26Slogan">
+                <h2 id="womanDay26Slogan_1">Тепла, любви и&nbsp;счастья!</h2>
+            </div>
+        `;
+
+        // Appends
+        this.bodyBlock.appendChild(this.womanDay26);
+        this.womanDay26.appendChild(this.container);
+    }
+
+    initAnimation() {
+        const womanDay26 = document.getElementById("womanDay26"),
+            womanDay26Back = document.getElementById("womanDay26Back"),
+            womanDay26Baloon = document.getElementById("womanDay26Baloon"),
+            womanDay26Slogan_1 = document.getElementById("womanDay26Slogan_1"),
+            womanDay26Slogan_2 = document.getElementById("womanDay26Slogan_2"),
+            womanDay26Slogan_3 = document.getElementById("womanDay26Slogan_3");
+        let tl = new gsap.timeline({
+            delay: 0.5,
+            onComplete: this.initHide(11),
+        });
+
+        tl.to(womanDay26, {
+            duration: 0.3,
+            delay: "1",
+            autoAlpha: 1,
+            zIndex: 9999,
+            // easy: "elastic.in(1,0.3)"
+        })
+            .from([womanDay26Back], {
+                duration: 0.8,
+                delay: "0",
+                autoAlpha: 0,
+            })
+            .from(womanDay26Baloon, {
+                duration: 6,
+                delay: "-0.2",
+                // autoAlpha: 0,
+                x: "100vmin",
+                scale: 1.5,
+                // easy: "bounce",
+            })
+            .from(
+                [
+                    womanDay26Slogan_1 /*, womanDay26Slogan_2, womanDay26Slogan_3*/,
+                ],
+                {
+                    duration: 3,
+                    delay: "-3",
+                    autoAlpha: 0,
+                    y: "30%",
+                    stagger: 0.3,
+                    // easy: "power2",
+                },
+            );
+    }
+
+    initHide(delay) {
+        let tl = new gsap.timeline({
+            delay: delay,
+            onComplete: () => {
+                setTimeout(() => {
+                    this.bodyBlock.removeChild(this.womanDay26);
+                }, 1000);
+            },
+        });
+
+        tl.to(this.womanDay26, {
+            autoAlpha: 0,
+            duration: 0.6,
+            delay: "-0.8",
+            display: "none",
+            zIndex: "-1",
+            ease: "power1",
+        });
+    }
+
+    initDev() {
+        this.womanDay26.style.opacity = 1;
+        this.womanDay26.style.visibility = "visible";
+        this.womanDay26.style.zIndex = 9999;
+    }
+}
+
+export { Event_08_03_26 };
